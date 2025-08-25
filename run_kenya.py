6d028bebb4b13488c39529c12d3813a33793ee49
Robyn Stuart <robyna.s@gmail.com>
2025-08-26 08:04:44 +1000

    add scens

--- run_kenya.py
+++ run_kenya.py
@@ -8,7 +8,7 @@ from fpsim import plotting as plt
 import pandas as pd
 from menstruation import Menstruation
 from education import Education
-import seaborn as sns
+import starsim as ss
 import pylab as pl
 import sciris as sc
 
@@ -24,42 +24,31 @@ def make_pars():
     pars = fp.make_fp_pars()  # For default pars
     pars.update_location('kenya')
 
-    # # Modify individual fecundity and exposure parameters
-    # # These adjust each woman's probability of conception and exposure to pregnancy.
-    # pars['fecundity_var_low'] = .8
-    # pars['fecundity_var_high'] = 3.25
-    # pars['exposure_factor'] = 3.5
-
-    # # Last free parameter, postpartum sexual activity correction or 'birth spacing preference'.
-    # # Pulls values from {location}/data/birth_spacing_pref.csv by default
-    # # Set all to 1 to reset.
-    # pars['spacing_pref']['preference'][:3] =  1  # Spacing of 0-6 months
-    # pars['spacing_pref']['preference'][3:6] = 1  # Spacing of 9-15 months
-    # pars['spacing_pref']['preference'][6:9] = 1  # Spacing of 18-24 months
-    # pars['spacing_pref']['preference'][9:] =  1  # Spacing of 27-36 months
+    # Modify individual fecundity and exposure parameters
+    # These adjust each woman's probability of conception and exposure to pregnancy.
+    pars['exposure_factor'] = 2
 
     # Adjust contraceptive choice parameters
-    pars['prob_use_year'] = 2020,         # Time trend intercept
-    pars['prob_use_trend_par'] = 0.03,    # Time trend parameter
-    pars['force_choose'] = False,         # Whether to force non-users to choose a method ('False' by default)
+    pars['prob_use_intercept'] = -1  # Intercept for the probability of using contraception
+
     # Weights assigned to dictate preferences between methods:
     method_weights = dict(
         pill=.7,
         iud=.5,
-        inj=2,
-        cond=.2,
+        inj=2.2,
+        cond=1.5,
         btl=1,
         wdraw=1,
-        impl=1.2,
-        othtrad=1,
-        othmod=1,
+        impl=3.5,
+        othtrad=.15,
+        othmod=.5,
     )
     pars['method_weights'] = np.array([*method_weights.values()])  # Weights for the methods in method_list in methods.py (excluding 'none', so starting with 'pill' and ending in 'othmod').
 
     return pars
 
 
-def make_sim(pars=None):
+def make_sim(pars=None, stop=2021):
     if pars is None:
         pars = make_pars()
 
@@ -72,6 +61,7 @@ def make_sim(pars=None):
     # Run the sim
     sim = fp.Sim(
         start=2000,
+        stop=stop,
         n_agents=1000,
         location='kenya',
         pars=pars,
@@ -112,14 +102,112 @@ def plot_by_age(sim, do_save=True, figs_directory='figures'):
     return
 
 
-if __name__ == '__main__':
-    # Run the simulation
-    sim = make_sim()
-    sim.run()
-    # sim = sc.loadobj('kenya.sim')
+def set_font(size=None, font='Libertinus Sans'):
+    sc.fonts(add=sc.thisdir(aspath=True) / 'assets' / 'LibertinusSans-Regular.otf')
+    sc.options(font=font, fontsize=size)
+    return
 
-    # Set options for plotting
-    # plt.plot_cpr(sim)
-    plt.plot_calib(sim)
-    plot_by_age(sim)
+
+if __name__ == '__main__':
+
+    to_run = [
+        # 'calib',
+        # 'plot_hmb',  # Plot the HMB results
+        'run_scenario',  # Run a scenario with interventions
+    ]
+    do_run = True
+
+    if 'calib' in to_run:
+        if do_run:
+            # Create simulation with parameters
+            sim = make_sim()
+            sim.run()
+            sc.saveobj('results/kenya_calib.sim', sim)
+        else:
+            sim = sc.loadobj('results/kenya_calib.sim')
+
+        # Set options for plotting
+        set_font(20)
+        plt.plot_calib(sim, single_fig=True)
+        plot_by_age(sim)
+
+    if 'plot_hmb' in to_run:
+        sim = sc.loadobj('results/kenya_calib.sim')
+
+        import numpy as np
+        t = sim.results.menstruation.timevec[::12]  # Take every 12th time point for speed
+        years = np.array([y.year for y in t])
+        si = sc.findfirst(years, 2010)
+        years = years[si:]
+        set_font(20)
+        fig, axes = pl.subplots(2, 2, figsize=(15, 9))
+        axes = axes.ravel()
+
+        res_to_plot = ['hmb', 'poor_mh', 'anemic', 'pain'] # , 'hyst']
+        labels = ['HMB prevalence\n(% of menstruating girls/women)', 'Poor MH prevalence\n(% of menstruating girls/women)', 'Anemia prevalence\n(% of menstruating girls/women)', 'Menstrual pain prevalence\n(% of menstruating girls/women)']  #, 'Hysterectomy']
+
+        for i, res in enumerate(res_to_plot):
+            ax = axes[i]
+            r0 = sim.results.menstruation[f'{res}_prev']
+            y0 = r0[::12][si:]
+            ax.plot(years, y0*100)
+            # ax.set_xticks(years[::2])
+            ax.set_title(labels[i])
+            ax.set_ylim(bottom=0, top=100)
+
+        pl.legend(fontsize=16, frameon=False, loc='upper left')
+        sc.figlayout()
+        sc.savefig('figures/hmb_results.png', dpi=150)
+
+    if 'run_scenario' in to_run:
+        if do_run:
+            s_base = make_sim(stop=2032)
+            s_i10 = make_sim(stop=2032)
+            s_i50 = make_sim(stop=2032)
+            from interventions import contra_hmb
+            s_i10['pars']['interventions'] = contra_hmb(prob=0.1)
+            s_i50['pars']['interventions'] = contra_hmb(prob=0.5)
+
+            m = ss.parallel([s_base, s_i10, s_i50], parallel=True)
+            s_base, s_i10, s_i50 = m.sims[:]  # Replace with run versions
+            # Save results
+            sc.saveobj('results/kenya_base.sim', s_base)
+            sc.saveobj('results/kenya_i10.sim', s_i10)
+            sc.saveobj('results/kenya_i50.sim', s_i50)
+
+        else:
+            s_base = sc.loadobj('results/kenya_base.sim')
+            s_i10 = sc.loadobj('results/kenya_i10.sim')
+            s_i50 = sc.loadobj('results/kenya_i50.sim')
+
+        # Plot
+        import numpy as np
+        t = s_base.results.menstruation.timevec[::12]  # Take every 12th time point for speed
+        years = np.array([y.year for y in t])
+        si = sc.findfirst(years, 2020)
+        years = years[si:]
+        set_font(20)
+        fig, axes = pl.subplots(2, 3, figsize=(15, 9))
+        axes = axes.ravel()
+
+        res_to_plot = ['hiud', 'hmb', 'poor_mh', 'anemic', 'pain']
+        labels = ['hIUD Usage', 'HMB ', 'Poor MH', 'Anemic', 'Pain']
+
+        for i, res in enumerate(res_to_plot):
+            ax = axes[i]
+            r0 = s_base.results.menstruation[f'{res}_prev']
+            y0 = r0[::12][si:]
+            y1 = s_i10.results.menstruation[f'{res}_prev'][::12][si:]
+            y2 = s_i50.results.menstruation[f'{res}_prev'][::12][si:]
+            ax.plot(years, y0*100, label='Baseline')
+            ax.plot(years, y1*100, label='hIUD 10% uptake')
+            ax.plot(years, y2*100, label='hIUD 50% uptake')
+            # ax.set_xticks(years[::2])
+            ax.axvline(x=2026, color='k', ls='--')
+            ax.set_title(labels[i])
+            ax.set_ylim(bottom=0, top=100)
+
+        pl.legend(fontsize=16, frameon=False, loc='upper left')
+        sc.figlayout()
+        sc.savefig('figures/hmb_scenario_results.png', dpi=150)
 
